983e8338b73ec14e5f8f9ad596c91d0a54b297b4
test <37450728+xiaoqix@users.noreply.github.com>
2024-02-11 16:57:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -217,16 +217,34 @@ def main(_user, _passwd, min_1, max_1):
     result = f"[{now}]\n账号：{user[:3]}****{user[7:]}\n修改步数（{step}）[" + response['message'] + "]\n"
     print(result)
     
-    ftkey = sys.argv[5]
-    postdata = {
-        "title":f"步数:{step}步，成功",
-        "desp":result
-    }
+   # ftkey = sys.argv[5]
+   # postdata = {
+   #     "title":f"步数:{step}步，成功",
+   #     "desp":result
+   # }
 
     
-    url = f'https://sctapi.ftqq.com/{ftkey}.send'
-    req = requests.post(url, data=postdata,headers=head).json()
-    print(req)
+   # url = f'https://sctapi.ftqq.com/{ftkey}.send'
+   # req = requests.post(url, data=postdata,headers=head).json()
+   # print(req)
+
+
+  #11111111111111111111111
+   de = "AT_0ofBQB18p7zFyRjPUhdEEiM37k8eXKs6"
+   url = 'https://wxpusher.zjiecode.com/api/send/message'
+   headers = {"content-type":"application/json"}
+   dat = {
+     "appToken":de,
+     "content":"Wxpusher祝你中秋节快乐!",
+     "summary":"消息摘要11",
+     "contentType":1,
+     "uids":[
+         "UID_9XH2D5JMlw6JeQM9qey3Cjc9qvoi"
+      ],
+      "url":"https://wxpusher.zjiecode.com",
+      "verifyPay":"False"
+    }
+    res = requests.post(url,json=dat,headers=headers)
     return result
 
 
